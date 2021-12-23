4dde5d07061cf6b6fb08719044f79406a5cec87b
Hodicq William <william.hodicq@groupe-cyllene.com>
2021-12-23 11:31:22 +0100

    feat: editor on text area up

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Articles;
 use App\Entity\Tags;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -17,7 +18,7 @@ class ArticlesType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('description', TextareaType::class)
+            ->add('description', CKEditorType::class)
             ->add('imageFile', FileType::class,[
                 'required' => false
             ])
